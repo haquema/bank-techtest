2cf7f055910e10b2bae9a16a372799780b26e0e4
mahaque <haquema@outlook.com>
2023-01-23 23:51:24 +0000

    write and pass third integration test: print statement for three deposit transactions and ensure the statement is sorted

--- lib/account.rb
+++ lib/account.rb
@@ -1,40 +1,40 @@
-
-
 class Account
   def initialize
     @transactions = []
-    @transaction_no = 0
     @balance = 0
-    @balance_record = {}
   end
 
   def deposit(date, amount)
-    new_deposit = Transaction.new("credit", date, amount)
-    @transactions << new_deposit
-    @transaction_no += 1
-    new_deposit.id=(@transaction_no)
-    @balance += amount
-    @balance_record[new_deposit.id] = @balance
+    transaction = Transaction.new("credit", date, amount)
+    @transactions << transaction
+    @balance += transaction.amount
   end
 
   def print_statement
     if @transactions.empty? 
       return "There's no transactions to display"
     else
-      header = "date || credit || debit || balance"
       statement = ""
-      @transactions.sort_by(&:date).reverse.each do |transaction|
-        statement += "\n#{transaction.format} #{@balance_record[transaction.id]}.00"
+      balance = @balance
+      sorted = sort_transactions(@transactions)
+      sorted.each do |transaction|
+        statement += "\n#{transaction.format} #{balance}.00"
+        balance -= transaction.amount
       end
+
+      return statement_header + statement
     end
-    
-    return header + statement
   end
 
-  # private
+  private
 
-  # def statement_header
-  #   return "date || credit || debit || balance"
-  # end
+  def statement_header
+    return "date || credit || debit || balance"
+  end
+
+  def sort_transactions(array)
+    array.sort_by(&:date).reverse
+  end
+      
 
 end
--- spec/account_transaction_integration_spec.rb
+++ spec/account_transaction_integration_spec.rb
@@ -15,10 +15,22 @@ RSpec.describe "integration" do
     it "prints a statement of that transaction in the desired format" do
       my_account = Account.new
       my_account.deposit("01/01/2023", 1000)
-      my_account.deposit("04/01/2023", 1000)
+      my_account.deposit("04/01/2023", 2000)
       result = my_account.print_statement
-      expect(result).to eq "date || credit || debit || balance\n04/01/2023 || 1000.00 || || 2000.00\n01/01/2023 || 1000.00 || || 1000.00"
+      expect(result).to eq "date || credit || debit || balance\n04/01/2023 || 2000.00 || || 3000.00\n01/01/2023 || 1000.00 || || 1000.00"
     end
   end
+
+  context "with three unordered deposit transactions recorded" do
+    it "prints a statement of that transaction in the desired format" do
+      my_account = Account.new
+      my_account.deposit("04/01/2023", 2000)
+      my_account.deposit("01/01/2023", 1000)
+      my_account.deposit("02/01/2023", 500)
+      result = my_account.print_statement
+      expect(result).to eq "date || credit || debit || balance\n04/01/2023 || 2000.00 || || 3500.00\n02/01/2023 || 500.00 || || 1500.00\n01/01/2023 || 1000.00 || || 1000.00"
+    end
+  end
+  
 
 end
