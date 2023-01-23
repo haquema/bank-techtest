c8d5cd32da53875b05d634eaa19270c7a48e4e17
mahaque <haquema@outlook.com>
2023-01-23 18:28:59 +0000

    add first integration test

--- spec/account_transaction_integration_spec.rb
+++ spec/account_transaction_integration_spec.rb
@@ -2,5 +2,13 @@ require "account"
 require "transaction"
 
 RSpec.describe "integration" do
+  context "with one deposit transaction recorded" do
+    it "prints a statement of that transaction in the desired format" do
+      my_account = Account.new
+      my_account.deposit("01/01/2023", 1000)
+      result = my_account.print_statement
+      expect(result).to eq "date || credit || debit || balance\n01/01/2023 || 1000.00 || || 1000.00"
+    end
+  end
 
 end
